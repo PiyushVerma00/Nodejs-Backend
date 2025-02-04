6cf2c9533640ddc448d9afc71ef70e065b222f1b
Piyush Verma <piyushverma0919@gamil.com>
2025-02-04 14:49:19 +0530

    all setup complete -routes,controllers,postman

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
-import express, { urlencoded } from "express"
-import Cors from "cors"
-import cookieParser from "cookie-parser"
+import express, { urlencoded } from "express";
+import Cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app  = express()
+const app = express();
 
-app.use(Cors({
+app.use(
+  Cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true,
-
-}))
+    credentials: true,
+  })
+);
 
 //middleware
-app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended:true, limit:"20kb"}))
-app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.route.js";
 
-export {app}
+app.use("/api/v1/users", userRouter);
+
+export { app };
